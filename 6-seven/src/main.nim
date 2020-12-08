ee6ec7b300d0fb814014506140b78820932cb819
ducharmemp <ducharmemp@gmail.com>
2020-12-07 20:35:28 -0500

    Cleanups and formatting

--- 6-seven/src/main.nim
+++ 6-seven/src/main.nim
@@ -12,35 +12,43 @@ type
     Graph* = object
       neighbors*: Table[string, seq[(int, RelationshipType, string)]]
 
+
 proc insertEntry*(g: var Graph, entry: string) =
   if entry notin g.neighbors:
     g.neighbors[entry] = @[]
+
 
 proc insertNeighbor*(g: var Graph, parent: string, neighbor: string, weight: int = 0) =
   g.insertEntry(neighbor)
   g.neighbors[parent].add((weight, RelationshipType.Parent, neighbor))
   g.neighbors[neighbor].add((weight, RelationshipType.Child, parent))
 
+
 proc walkFrom*(g: Graph, entry: string, typ: RelationshipType = RelationshipType.Child): seq[string] =
   var stack: seq[string] = @[entry]
   var path: seq[string] = @[]
+
   while stack.len != 0:
     let tail = pop(stack)
+
     if tail notin path and tail != entry:
       path.add(tail)
 
     for neighbor in g.neighbors[tail].filter(neighbor => neighbor[1] == typ):
       if neighbor[2] in path:
         continue
+      
       stack.add(neighbor[2])
 
   path
+
 
 proc sumFrom*(g: Graph, entry: string, typ: RelationshipType = RelationshipType.Child): int =
   var seen = @[entry]
   
   proc inner(entry: string, typ: RelationshipType, seen: var seq[string]): int =
     var total = 1
+
     for neighbor in g.neighbors[entry].filter(neighbor => neighbor[1] == typ):
       let innerVal = inner(neighbor[2], typ, seen=seen)
       echo neighbor, " ", total, " ", innerVal
@@ -52,43 +60,53 @@ proc sumFrom*(g: Graph, entry: string, typ: RelationshipType = RelationshipType.
   inner(entry, typ, seen) - 1
 
 
-proc solution1(lines: seq[string]): string =
+proc solution1(lines: seq[string]): int =
   var graph = Graph(neighbors: initTable[string, seq[(int, RelationshipType, string)]]())
   let toFind = "shiny gold"
+
   for line in lines:
     if line == "":
       continue
+
     let items = split(line, "contain", 1)
     let parent = strip(items[0].replace(re"[^a-zA-Z ]+|bags?", ""))
     let children = split(items[1], ",").map(child => strip(child.replace(re"[^a-zA-Z ]+|bags?", "")))
+
     graph.insertEntry(parent)
+
     for child in children:
       if child == "no other":
         continue
       graph.insertNeighbor(parent, child)
     
   let entries = graph.walkFrom(toFind)
-  echo toHashSet(entries).len
+  toHashSet(entries).len
 
-proc solution2(lines: seq[string]): string =
+
+proc solution2(lines: seq[string]): int =
   var graph = Graph(neighbors: initTable[string, seq[(int, RelationshipType, string)]]())
   let toFind = "shiny gold"
+
   for line in lines:
     if line == "":
       continue
+
     let items = split(line, "contain", 1)
     let parent = strip(items[0].replace(re"[^a-zA-Z ]+|bags?", ""))
     let children = split(items[1], ",").map(child => strip(child.replace(re"[^a-zA-Z0-9 ]+|bags?", ""))).map(child => split(child, " ", 1))
+
     graph.insertEntry(parent)
+
     for child in children:
       if child[0] == "no" and child[1] ==  "other":
         continue
+
       graph.insertNeighbor(parent, child[1], weight=parseInt(child[0]))
     
-  # echo graph.neighbors["shiny gold"]
-  # echo graph.neighbors["striped beige"]
+
   let total = graph.sumFrom(toFind, typ = RelationshipType.Parent)
-  echo total
+  total
+
 
 proc main(): int =
   let input = readFile("input.txt")
@@ -96,5 +114,6 @@ proc main(): int =
   echo solution1(lines)
   echo solution2(lines)
   0
+
 
 discard main()
